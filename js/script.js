b85b11766c48f6e2f815765897499bec45287d58
AnggaTP <anggapriantoko2000@gmail.com>
2023-07-19 10:16:01 +0700

    fix some funtion to add book and display library

--- js/script.js
+++ js/script.js
@@ -6,61 +6,57 @@ function Book(title, author, pages){
     this.pages = pages
 }
 
-Book.prototype.addBook = function(){
-    myLibrary.push({title:this.title,author:this.author,pages:this.pages})
+function addBookToLibrary(){
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#pages').value
+
+    let newBook = new Book(title, author, pages)
+    myLibrary.push(newBook)
+    render()
 }
 
-function updateDisplay (){
-    if(myLibrary[myLibrary.length-1].title===''&& myLibrary[myLibrary.length-1].author==='' && myLibrary[myLibrary.length-1].pages ===''){
-        return
+function removeBook(index){
+    myLibrary.splice(index, 1)
+    render()
+}
+
+function render(){
+    let output = document.querySelector('.output')
+    output.innerHTML = ''
+    for(let i = 0;i<myLibrary.length;i++){
+        let book = myLibrary[i]
+        let card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        card.innerHTML = `
+        <div class='title'>
+            <div>Book Title</div>
+            <div class='content'>${book.title}</div>
+        </div>
+        <div class='author'>
+            <div>Author</div>
+            <div class='content'>${book.author}</div>
+        </div>
+        <div class='totalPages'>
+            <div>Total Pages</div>
+            <div class='content'>${book.pages} Pages</div>
+        </div>
+        <div class='removeButton'>
+            <button onclick=removeBook(${i})>Remove</button>
+        </div>
+        `
+        output.append(card)
     }
-    const output = document.querySelector('.output')
-    const card = document.createElement('div')
-    card.style.padding = '30px'
-    card.style.height = '300px'
-    card.style.backgroundColor = '#004643'
-    card.style.borderRadius = '5px'
-    card.style.display = 'grid'
-    card.style.gridTemplateRows = 'repeat(4, 1fr)'    
-    const titleText = document.createElement('div')
-    titleText.textContent = `Book Title : ` + myLibrary[myLibrary.length-1].title
-    titleText.style.color = 'whitesmoke'
-    const authorText = document.createElement('div')
-    authorText.textContent = `Author : ` + myLibrary[myLibrary.length-1].author
-    authorText.style.color = 'whitesmoke'
-    const pageText = document.createElement('div')
-    pageText.textContent = `Total Page : ` + myLibrary[myLibrary.length-1].pages.toString() + ` Pages`
-    pageText.style.color = 'whitesmoke'
-    const button = document.createElement('button')
-    button.textContent = 'Remove'
-    button.style.backgroundColor = '#f9bc60'
-    button.style.color = '#001e1d'
-    button.style.border = 'none'
-    button.style.height = '40px'
-    card.append(titleText)
-    card.append(authorText)
-    card.append(pageText)
-    card.append(button)
-    output.append(card)
-
 }
-
 
 const showForm = document.querySelector('[add-book]')
-const formCard = document.querySelector('.form-card')
-const titleText = document.querySelector('#title')
-const authorText = document.querySelector('#author')
-const pageText = document.querySelector('#pages')
+const formCard = document.querySelector('.form-card') //for display form
 const submitBtn = document.querySelector('[submit-btn]')
-const form = document.querySelector('form')
-
-
+const form = document.querySelector('form') // for reset form value
 
 submitBtn.addEventListener('click', (event)=>{
-    const book  = new Book(titleText.value, authorText.value, pageText.value)
-    book.addBook()
+    addBookToLibrary()
     formCard.style.display = 'none'
-    updateDisplay()
     form.reset()
     event.preventDefault()
 })
@@ -68,4 +64,3 @@ submitBtn.addEventListener('click', (event)=>{
 showForm.addEventListener('click', ()=>{
     formCard.style.display = 'grid'
 })
-
